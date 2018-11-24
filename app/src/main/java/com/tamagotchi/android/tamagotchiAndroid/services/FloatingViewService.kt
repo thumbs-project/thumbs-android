edb8ce83cbee90a013da7a2fb5de261438430a6c
eldeni <eldeniyenden@gmail.com>
2018-11-25 00:49:57 +0900

    feat: add WidgetBase that has util methods for creating new Widgets
    - Widgets are supposed to be generated in general fashion. However, at this moment,
    only a part of construction is generatlized. [refactor needed in the future]
    
    Fixes https://github.com/tamagotchi-project/tamagotchi-project/issues/11

--- app/src/main/java/com/tamagotchi/android/tamagotchiAndroid/services/FloatingViewService.kt
+++ app/src/main/java/com/tamagotchi/android/tamagotchiAndroid/services/FloatingViewService.kt
@@ -10,25 +10,16 @@ import android.view.*
 import android.view.animation.Animation
 import com.tamagotchi.android.tamagotchiAndroid.R
 import android.view.GestureDetector.SimpleOnGestureListener
-
-
-
+import com.tamagotchi.android.tamagotchiAndroid.views.ItemWidget
+import com.tamagotchi.android.tamagotchiAndroid.views.MainWidget
 
 class FloatingViewService : Service() {
-
   val windowManager by lazy {
     getSystemService(WINDOW_SERVICE) as WindowManager
   }
 
-  val singleTabConfirm by lazy {
-    GestureDetector(this,SingleTapConfirm())
-  }
-  var collapsedView: View? = null
-  var expandedView: View? = null
-  var movingAnim: Animation? = null
   var floatingView: View? = null
   var buttonClose: View? = null
-
 
   override fun onBind(intent: Intent?): IBinder? = null
 
@@ -38,123 +29,31 @@ class FloatingViewService : Service() {
   }
 
   fun init() {
-    floatingView = LayoutInflater.from(this).inflate(R.layout.layout_floating_widget, null)
-
-    var mParams = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
-      WindowManager.LayoutParams(
-        WindowManager.LayoutParams.WRAP_CONTENT,
-        WindowManager.LayoutParams.WRAP_CONTENT,
-        WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
-        WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
-        PixelFormat.TRANSLUCENT
-      )
-    } else {
-      WindowManager.LayoutParams(
-        WindowManager.LayoutParams.WRAP_CONTENT,
-        WindowManager.LayoutParams.WRAP_CONTENT,
-        WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
-        WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
-        PixelFormat.TRANSLUCENT
-      )
-    }
-
-    windowManager.addView(floatingView, mParams)
-
-    collapsedView = floatingView?.findViewById(R.id.layoutCollapsed)
-    expandedView = floatingView?.findViewById(R.id.layoutExpanded)
-    buttonClose = floatingView?.findViewById(R.id.buttonClose)
-
-    expandedView?.setOnClickListener {
-      //showLayout()
-    }
-
-    buttonClose?.setOnClickListener {
-      stopSelf()
-    }
-
-
-    floatingView?.setOnTouchListener(object : View.OnTouchListener {
-      private var initialX: Int = 0
-      private var initialY: Int = 0
-      private var initialTouchX: Float = 0.toFloat()
-      private var initialTouchY: Float = 0.toFloat()
-
-      override fun onTouch(v: View, event: MotionEvent): Boolean {
-
-
-        if(singleTabConfirm.onTouchEvent(event)){
-          // single tab
-          Log.d("floatingView","widget clicked")
-
-          return true
-        }else{
-          when (event.action) {
-            MotionEvent.ACTION_DOWN -> {
-              initialX = mParams.x
-              initialY = mParams.y
-              initialTouchX = event.rawX
-              initialTouchY = event.rawY
-              return true
-            }
-
-            MotionEvent.ACTION_UP -> {
-              //when the drag is ended switching the state of the widget
-              //hideLayout()
-              //return true
-            }
-
-            MotionEvent.ACTION_MOVE -> {
-              //this code is helping the widget to move around the screen with fingers
-              mParams.x = initialX + (event.rawX - initialTouchX).toInt()
-              mParams.y = initialY + (event.rawY - initialTouchY).toInt()
-              windowManager.updateViewLayout(floatingView, mParams)
-              return true
-            }
-          }
-        }
-        return false
-      }
-    })
+    MainWidget(this, windowManager);
+    ItemWidget(this, windowManager);
   }
-
-  fun showLayout(){
-    collapsedView?.visibility = (View.VISIBLE)
-    expandedView?.visibility = (View.GONE)
-  }
-  fun hideLayout(){
-    collapsedView?.visibility = (View.GONE)
-    expandedView?.visibility = (View.VISIBLE)
-  }
-
 
   override fun onDestroy() {
     super.onDestroy()
     floatingView?.let {
-      windowManager?.removeView(it)
+      windowManager.removeView(it)
     }
   }
 }
 
 //setAnimationListener 코드
 /*
-    movingAnim = AnimationUtils.loadAnimation(this, R.anim.animation)
-    movingAnim?.setAnimationListener(object : Animation.AnimationListener {
-      override fun onAnimationStart(animation: Animation) {
+  movingAnim = AnimationUtils.loadAnimation(this, R.anim.animation)
+  movingAnim?.setAnimationListener(object : Animation.AnimationListener {
+    override fun onAnimationStart(animation: Animation) {
 
-      }
+    }
 
-      override fun onAnimationEnd(animation: Animation) {
-        Toast.makeText(applicationContext, "애니메이션 종료됨.", Toast.LENGTH_LONG).show()
-      }
+    override fun onAnimationEnd(animation: Animation) {
+      Toast.makeText(applicationContext, "애니메이션 종료됨.", Toast.LENGTH_LONG).show()
+    }
 
-      override fun onAnimationRepeat(animation: Animation) {
-
-      }
-    })*/
-
-class SingleTapConfirm : SimpleOnGestureListener() {
-
-  override fun onSingleTapUp(event: MotionEvent): Boolean {
-    return true
-  }
-}
+    override fun onAnimationRepeat(animation: Animation) {
+    }
+  })
+*/
