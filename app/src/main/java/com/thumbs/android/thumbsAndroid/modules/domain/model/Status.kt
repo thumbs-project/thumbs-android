741d457d0fad6abeb6b4ffbb055be6a68f28ca1d
eldeni <eldeniyenden@gmail.com>
2018-11-25 09:02:35 +0900

    feat: set NetworkConnect use development endpoint
    - `Status` model is added, so is StatusRequeestParam.
    - handleSingleClick(), within which network request takes place, is defined.
    
    Fixes https://github.com/thumbs-project/thumbs-project/issues/19

---
+++ app/src/main/java/com/thumbs/android/thumbsAndroid/modules/domain/model/Status.kt
@@ -0,0 +1,7 @@
+package com.thumbs.android.thumbsAndroid.modules.domain.model
+
+data class Status(
+  val property: String,
+  val status_change: String?,
+  val request_id: Long
+)
--- app/src/main/java/com/thumbs/android/thumbsAndroid/modules/domain/model/User.kt
+++ app/src/main/java/com/thumbs/android/thumbsAndroid/modules/domain/model/User.kt
@@ -1,7 +1,5 @@
 package com.thumbs.android.thumbsAndroid.modules.domain.model
 
-
-
 data class User(
   var id : String,
   var url : String
