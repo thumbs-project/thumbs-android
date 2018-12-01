31b2c580b454042fe8b6d60764794fe54c50e3a6
eldeni <eldeniyenden@gmail.com>
2018-12-01 20:13:16 +0900

    refactor: rename FloatingService as `ControllerServce`
    - Some code styles are refined as well.
    
    Fixes https://github.com/thumbs-project/thumbs-project/issues/20

---
+++ app/src/main/java/com/thumbs/android/thumbsAndroid/services/ControllerService.kt
@@ -0,0 +1,39 @@
+package com.thumbs.android.thumbsAndroid.services
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.view.*
+import com.thumbs.android.thumbsAndroid.views.ItemWidget
+import com.thumbs.android.thumbsAndroid.views.MainWidget
+import com.thumbs.android.thumbsAndroid.views.UpdownWidget
+
+class ControllerService : Service() {
+  var floatingView: View? = null
+  val windowManager by lazy {
+    getSystemService(Context.WINDOW_SERVICE) as WindowManager
+  }
+
+  override fun onCreate() {
+    super.onCreate()
+    init()
+  }
+
+  override fun onBind(intent: Intent?): IBinder? {
+    return null
+  }
+
+  fun init() {
+    MainWidget(this, windowManager)
+    ItemWidget(this, windowManager)
+    UpdownWidget(this, windowManager)
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    floatingView?.let {
+      windowManager.removeView(it)
+    }
+  }
+}
