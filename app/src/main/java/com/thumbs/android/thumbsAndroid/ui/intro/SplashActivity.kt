371287c4b1035b364eae637e28aa58971947aeb7
LenKIM <joenggyu0@gmail.com>
2019-02-06 03:08:05 +0900

    fix: ask Permission on SplashActivity
    등록할 때 물어보던 권한 문제를 SplashActivity로 가져옴
    
    Fixes thumbs-project/thumbs-project#38

--- app/src/main/java/com/thumbs/android/thumbsAndroid/ui/intro/SplashActivity.kt
+++ app/src/main/java/com/thumbs/android/thumbsAndroid/ui/intro/SplashActivity.kt
@@ -1,20 +1,42 @@
 package com.thumbs.android.thumbsAndroid.ui.intro
 
-import android.os.Bundle
 import android.content.Intent
-import com.thumbs.android.thumbsAndroid.ui.register.RegisterActivity
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
 import com.thumbs.android.thumbsAndroid.ui.base.BaseActivity
+import com.thumbs.android.thumbsAndroid.ui.register.RegisterActivity
 
 
 class SplashActivity : BaseActivity() {
 
+    private val CODE_OVERLAY_PERMISSION = 2002
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        startActivityIfPermissionPass()
+    }
 
-        val intent = Intent(this, RegisterActivity::class.java)
-        startActivity(intent)
+    private fun startActivityIfPermissionPass() {
+        when {
+            Settings.canDrawOverlays(this) -> {
+                val intent = Intent(this, RegisterActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            else -> {
+                checkPermission()
+                Toast.makeText(this, "You need System Alert Window Permission to do this", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
 
-        finish()
+    private fun checkPermission() {
+        Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName")).let{
+            startActivityForResult(it, CODE_OVERLAY_PERMISSION)
+        }
     }
 
     override fun startInject() {
