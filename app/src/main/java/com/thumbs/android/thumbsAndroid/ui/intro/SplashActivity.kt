8d4483d0d7b3e6876ef4a408f86f1a8f768d40ed
LenKIM <joenggyu0@gmail.com>
2019-02-09 01:14:01 +0900

    feat: inital animation

--- app/src/main/java/com/thumbs/android/thumbsAndroid/ui/intro/SplashActivity.kt
+++ app/src/main/java/com/thumbs/android/thumbsAndroid/ui/intro/SplashActivity.kt
@@ -1,21 +1,83 @@
 package com.thumbs.android.thumbsAndroid.ui.intro
 
+import android.animation.ValueAnimator
+import android.animation.ValueAnimator.REVERSE
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
 import android.provider.Settings
+import android.view.animation.AccelerateInterpolator
 import android.widget.Toast
+import com.thumbs.android.thumbsAndroid.R
 import com.thumbs.android.thumbsAndroid.model.Thumb
 import com.thumbs.android.thumbsAndroid.services.ControllerService
 import com.thumbs.android.thumbsAndroid.ui.base.BaseActivity
 import com.thumbs.android.thumbsAndroid.ui.register.RegisterActivity
 import com.thumbs.android.thumbsAndroid.ui.status.StatusActivity
+import kotlinx.android.synthetic.main.activity_splash.*
 import org.koin.android.ext.android.inject
 
 
 class SplashActivity : BaseActivity(), SplashContract.SplashView {
 
-    val presenter  by inject<SplashContract.SplashUserActionListerner>()
+
+    val presenter by inject<SplashContract.SplashUserActionListerner>()
+
+    private val CODE_OVERLAY_PERMISSION = 2002
+
+    override fun startInject() {
+        presenter.attachView(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        startActivityIfPermissionPass()
+        // 1
+// 2
+
+
+    }
+
+    private fun startActivityIfPermissionPass() {
+
+        val runnable = Runnable {
+
+            ValueAnimator.ofFloat(0f, -0.3f).apply {
+                duration = 10000
+                addUpdateListener {
+                    it.interpolator = AccelerateInterpolator()
+                    splash_iv.y = it.animatedValue as Float
+                    it.repeatMode = REVERSE
+                    it.repeatCount = 2
+                }
+                start()
+            }
+
+            when {
+                Settings.canDrawOverlays(this) -> {
+                    presenter.loadThumbsData()
+                }
+                else -> {
+                    checkPermission()
+                    Toast.makeText(this, "You need System Alert Window Permission to do this", Toast.LENGTH_SHORT)
+                        .show()
+                    finish()
+                }
+            }
+        }
+        Handler().apply {
+            postDelayed(runnable, 10000)
+        }
+    }
+
+    private fun checkPermission() {
+        Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName"))
+            .let {
+                startActivityForResult(it, CODE_OVERLAY_PERMISSION)
+            }
+    }
 
     override fun success(thumbs: Thumb) {
         val intent = Intent(this, StatusActivity::class.java)
@@ -29,35 +91,4 @@ class SplashActivity : BaseActivity(), SplashContract.SplashView {
         startActivity(intent)
         finish()
     }
-
-    private val CODE_OVERLAY_PERMISSION = 2002
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        startActivityIfPermissionPass()
-    }
-
-    private fun startActivityIfPermissionPass() {
-        when {
-            Settings.canDrawOverlays(this) -> {
-                presenter.loadThumbsData()
-            }
-            else -> {
-                checkPermission()
-                Toast.makeText(this, "You need System Alert Window Permission to do this", Toast.LENGTH_SHORT).show()
-                finish()
-            }
-        }
-    }
-
-    private fun checkPermission() {
-        Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName")).let{
-            startActivityForResult(it, CODE_OVERLAY_PERMISSION)
-        }
-    }
-
-    override fun startInject() {
-        presenter.attachView(this)
-    }
-
 }
